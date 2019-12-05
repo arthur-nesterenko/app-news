68ce4a2e4ae1f0803849aa59b8f66bf3c373add9
arthur-nesterenko <arthur-nesterenko@users.noreply.github.com>
2019-12-05 14:46:55 +0000

    Automated deployment: Thu Dec  5 14:46:55 UTC 2019 7b1c8da0c8183b2cb8726a75f023fc6307336a71

---
+++ precache-manifest.adf9eeb37df865723842971bc916853b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "431adc4a7708c050b0cc289e480e2599",
+    "url": "/app-news/index.html"
+  },
+  {
+    "revision": "0ffe6d60b2457466b98d",
+    "url": "/app-news/static/css/4.7894f277.chunk.css"
+  },
+  {
+    "revision": "c60af1af53830e98566b",
+    "url": "/app-news/static/css/main.21ca423d.chunk.css"
+  },
+  {
+    "revision": "06b14c4c8e3da094483b",
+    "url": "/app-news/static/js/0.993330df.chunk.js"
+  },
+  {
+    "revision": "282b2d82c9cfafea4d76",
+    "url": "/app-news/static/js/3.fa1a3494.chunk.js"
+  },
+  {
+    "revision": "0ffe6d60b2457466b98d",
+    "url": "/app-news/static/js/4.90288fda.chunk.js"
+  },
+  {
+    "revision": "37913e775569fa414aa9",
+    "url": "/app-news/static/js/5.0af3a76c.chunk.js"
+  },
+  {
+    "revision": "fc810ed56ce139a17d43",
+    "url": "/app-news/static/js/6.22ff01f9.chunk.js"
+  },
+  {
+    "revision": "23211386c20c21cdb876",
+    "url": "/app-news/static/js/7.32612531.chunk.js"
+  },
+  {
+    "revision": "32e6e57e44f63efcae0d",
+    "url": "/app-news/static/js/8.41d6ef58.chunk.js"
+  },
+  {
+    "revision": "6bffb94f6b25602f12d8",
+    "url": "/app-news/static/js/9.a0d53da3.chunk.js"
+  },
+  {
+    "revision": "c60af1af53830e98566b",
+    "url": "/app-news/static/js/main.089c328d.chunk.js"
+  },
+  {
+    "revision": "2c1d05bcfec03726e08f",
+    "url": "/app-news/static/js/runtime-main.edaa8b13.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/app-news/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/app-news/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/app-news/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/app-news/static/media/slick.f97e3bbf.svg"
+  }
+]);
