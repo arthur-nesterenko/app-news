487e38648422106fd9546cca6dadc38be4f96831
Arthur <nesterenko19@gmail.com>
2019-12-05 16:33:28 +0200

    :star: feature(categories)
    add Categories screen
    add Single category screen

---
+++ src/screens/categories/screen.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useParams} from "react-router";
+import {useAsync} from "react-use";
+import newsApi from "services/news/api";
+import Slider from "shared/components/slider";
+import Loader from "shared/components/loader";
+
+const categories = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology']
+const PAGE_SIZE = 5;
+const getCategories = async (code: any) => {
+    const promises = categories.map((category: any) => newsApi.fetch({category, country: code, pageSize: PAGE_SIZE}))
+    return Promise.all(promises)
+}
+
+const Screen = () => {
+
+    const {code} = useParams()
+    const state = useAsync(async () => {
+        return await getCategories(code);
+    }, [code]);
+
+    console.log('state', state)
+
+
+    return <div className='mb-5 px-12'>
+        <h1 className='sm:text-center md:text-left text-2xl'>Top 5 news by categories from <span
+            className='uppercase'>{code}</span></h1>
+        <div className='border-1 rounded'>
+            {state.loading ? <Loader/> : state.value && state.value.map((items, index) => <Slider key={index}
+                                                                                                  items={items}
+                                                                                                  category={categories[index]}/>)}
+        </div>
+    </div>
+}
+
+export default Screen;
---
+++ src/screens/category/screen.tsx
@@ -0,0 +1,36 @@
+import React, {Fragment} from 'react'
+import {useHistory, useParams} from 'react-router-dom'
+import {useAsync} from "react-use";
+import newsApi, {Category} from "services/news";
+import NewsList from "../../shared/components/news-list";
+import Loader from 'shared/components/loader'
+
+const Screen = () => {
+    const {goBack} = useHistory()
+    const {code, category} = useParams<{
+        category: Category,
+        code: string
+    }>();
+
+    const state = useAsync(async () => {
+        return await newsApi.fetch({
+            country: code,
+            //@ts-ignore
+            category
+        });
+    }, [code]);
+
+
+    return <Fragment>
+        <h1 className='text-base text-3xl capitalize my-2'>Top <strong>{category} </strong> news
+            from {code === 'us' ? 'United States' : 'Great Britain'}</h1>
+        {state.loading ? <Loader/> : <NewsList items={state.value || []}/>}
+        <div className='flex justify-start items-center py-5'>
+            <button onClick={goBack} className='cursor-pointer outline-none focus:outline-none'>
+                <span className='text-base'> Back to list</span>
+            </button>
+        </div>
+    </Fragment>
+}
+
+export default Screen;
